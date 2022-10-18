201f9f7c83c4a575819a6c5cd0aa7004c00e6ab4
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-10-18 18:44:04 -0300

    fix: responsivity

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ToolbarModule} from 'primeng/toolbar';
+
+import { ToolbarModule } from 'primeng/toolbar';
+
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { SomosComponent } from './somos/somos.component';
 import { TitleComponent } from './title/title.component';
-import {FieldsetModule} from 'primeng/fieldset';
-import {SidebarModule} from 'primeng/sidebar';
-import {ButtonModule} from 'primeng/button';
-import {MenuItem, PrimeIcons} from 'primeng/api';
+
+import { FieldsetModule } from 'primeng/fieldset';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @NgModule({
   declarations: [
